7dfb91be1d68332482f5e0f5fb8696209ec514e7
John Missal <jmissal@ucdavis.edu>
2024-02-28 20:03:56 -0800

    phase1

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,6 +7,29 @@
 #include "disk.h"
 #include "fs.h"
 
+#define FAT_EOC 0xFFFF 
+
+struct SuperBlock {
+    char signature[8];         // File system signature "ECS150FS"
+    uint16_t total_blocks;     // Total number of blocks in the virtual disk
+    uint16_t root_dir_index;   // Block index of the root directory
+    uint16_t data_start_index; // Block index where data blocks start
+    uint16_t data_blocks;      // Total number of data blocks
+    uint8_t fat_blocks;        // Number of blocks for the FAT
+    uint8_t unused[4079];      // Padding to make the superblock size 4096 bytes
+} __attribute__((packed));
+
+typedef struct RootDirectory {
+    char filename[FS_FILENAME_LEN]; // Filename (including NULL character)
+    uint32_t file_size;             // Size of the file in bytes
+    uint16_t first_data_block;      // Index of the first data block
+    uint8_t unused[10];             // Unused/Padding to align to 32 bytes
+} __attribute__((packed));
+
+static struct SuperBlock superblock; 
+static struct RootDirectory *root_directory = NULL; 
+static uint16_t *fat16 = NULL;       
+
 /* TODO: Phase 1 */
 
 int fs_mount(const char *diskname) 
